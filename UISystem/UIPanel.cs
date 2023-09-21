97bb2533a935f685f776a99f66e6e05d84214ae2
watanabe@marida <watanabe.everystudio@gmail.com>
2023-09-21 18:03:42 +0900

    anogamelibリポジトリ作成

---
+++ UISystem/UIPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanel : MonoBehaviour
+{
+    public void Initialize()
+    {
+        initialize();
+    }
+
+    protected virtual void initialize() { }
+
+    public void Shutdown()
+    {
+        shutdown();
+    }
+
+    protected virtual void shutdown() { }
+}
