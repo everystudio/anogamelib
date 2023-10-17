3feb6850bb968705113fe421aea322e93e7308b1
watanabe@marida <watanabe.everystudio@gmail.com>
2023-10-17 10:58:52 +0900

    シングルトン処理の修正

--- Utility/Singleton.cs
+++ Utility/Singleton.cs
@@ -1,43 +1,79 @@
-﻿using UnityEngine;
-namespace anogame
-{
-    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
-    {
-        protected static T _instance;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-        public static T Instance
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private bool _isInitialized = false;
+    public bool dontDestroyOnLoad = false;
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
         {
-            get
+            if (_instance == null)
             {
+                Debug.Log(Type.GetType(typeof(T).ToString()) + "のインスタンスを生成します");
+                _instance = FindObjectOfType<T>();
                 if (_instance == null)
                 {
-                    _instance = FindObjectOfType<T>();
-                    if (_instance == null)
-                    {
-                        GameObject obj = new GameObject();
-                        _instance = obj.AddComponent<T>();
-                    }
-                    _instance.Initialize();
+                    Debug.LogError("インスタンスが存在しません");
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
                 }
-
-                return _instance;
+                if (_instance._isInitialized == false)
+                {
+                    _instance.initialize();
+                }
             }
+
+            return _instance;
         }
-
-        protected virtual void Awake()
-        {
-            if (!Application.isPlaying)
-            {
-                return;
-            }
-
-            if (_instance != this)
-            {
-                _instance = this as T;
-                _instance.Initialize();
-            }
-        }
-
-        public virtual void Initialize() { }
     }
+
+    protected virtual void Awake()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        if (_instance == null)
+        {
+            _instance = this as T;
+            initialize();
+        }
+        else if (_instance == this)
+        {
+            //Debug.Log("外部からのアクセスで初期化済み");
+        }
+        else
+        {
+            Debug.LogError(Type.GetType(typeof(T).ToString()) + "のシングルトン");
+            Debug.LogError("既にインスタンスが存在するため、削除します");
+            Destroy(gameObject);
+        }
+    }
+
+    private void initialize()
+    {
+        if (_isInitialized)
+        {
+            return;
+        }
+
+        // 継承先のInitializeの中でdontDestroyOnLoadをtrueにしても間に合うように変更
+        _instance.Initialize();
+
+        if (dontDestroyOnLoad)
+        {
+            transform.SetParent(null);
+            Debug.Log("dontDestroyOnLoad:" + gameObject.name);
+            DontDestroyOnLoad(gameObject);
+        }
+        _isInitialized = true;
+    }
+
+    public virtual void Initialize() { }
 }
